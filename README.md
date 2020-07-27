5d61bdde3788931838a8bd7a5a872c4fdd7e01bb
Ralph Ritoch <rritoch@gmail.com>
2020-07-28 04:47:14 +0800

    Added security update

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Communicate [Pandas](https://pypi.org/project/pandas/) DataFrame over [ZeroMQ](h
 ## API
 
 ```
-send_dataframe(socket, dataframe, flags=0)
+send_dataframe(socket, dataframe, secret='', alg='sha256', flags=0)
 ```
 
 ```
-recv_dataframe(socket, flags=0)
+recv_dataframe(socket, secret='', alg='sha256', flags=0)
 ```
 
 ### Server
@@ -30,3 +30,8 @@ import pandas_zmq
 # ...
 df = pandas_zmq.recv_dataframe(socket)
 ```
+
+
+## Security
+
+WARNING: There are reported security issues with pickle, which is the backend serialization for this feature. A signature using a shared secret and a configurable hash algorithm has been added to mitigate this risk which will attempt to verify the data has not been altered. [See pickle documentation](https://docs.python.org/3/library/pickle.html)
--- pandas_zmq/_zmq.py
+++ pandas_zmq/_zmq.py
@@ -1,18 +1,31 @@
 
 import zlib
 import pickle
+import hashlib
 from zmq import *
 
 
-def send_dataframe(socket, obj, flags=0):
+def send_dataframe(socket, obj, secret='', flags=0, alg='sha256'):
     """send pandas dataframe***"""
+
     p = pickle.dumps(obj, -1)
-    z = zlib.compress(p)
+    a = hashlib.new(alg)
+    a.update(secret.encode() + p)
+    h = a.hexdigest().upper()
+    z = zlib.compress(h.encode() + b'\n' + p)
     return socket.send(z, flags=flags)
 
 
-def recv_dataframe(socket, flags=0):
+def recv_dataframe(socket, secret='', flags=0, alg='sha256'):
     """receive pandas dataframe***"""
     z = socket.recv(flags)
-    p = zlib.decompress(z)
+    s = zlib.decompress(z)
+    h = s[:s.find(b'\n')]
+    p = s[(s.find(b'\n')+1):]
+    a = hashlib.new(alg)
+    a.update(secret.encode() + p)
+
+    if a.hexdigest().upper() != h.decode():
+        raise Exception("Signature mismatch")
+
     return pickle.loads(p)
