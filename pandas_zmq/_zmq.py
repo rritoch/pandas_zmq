d428fb049a52a2dc7e850156bfa2a0d16831177b
Ralph Ritoch <rritoch@gmail.com>
2020-07-28 16:33:00 +0800

    Improved hash implementation

--- pandas_zmq/_zmq.py
+++ pandas_zmq/_zmq.py
@@ -5,27 +5,47 @@ import hashlib
 from zmq import *
 
 
+def h4mac(key, message, alg):
+    '''
+        4 x Hashed message authentication code
+
+        Author: Ralph Ritoch
+        H(H(K) ^ H(M) ^ H(K + M + K))
+    '''
+    a1 = hashlib.new(alg)
+    a2 = hashlib.new(alg)
+    a3 = hashlib.new(alg)
+    a4 = hashlib.new(alg)
+
+    a1.update(key)
+    a2.update(message)
+    a3.update(key + message + key)
+
+    a4.update(bytes([_a ^ _b ^ _c for _a, _b, _c in zip(
+        a1.digest(), a2.digest(), a3.digest())]))
+
+    return a4
+
+
 def send_dataframe(socket, obj, secret='', flags=0, alg='sha256'):
     """send pandas dataframe***"""
 
     p = pickle.dumps(obj, -1)
-    a = hashlib.new(alg)
-    a.update(secret.encode() + p)
-    h = a.hexdigest().upper()
-    z = zlib.compress(h.encode() + b'\n' + p)
+    h = h4mac(secret.encode(), p, alg).hexdigest().upper()
+    s = h.encode() + b'\n' + p
+    z = zlib.compress(s)
     return socket.send(z, flags=flags)
 
 
 def recv_dataframe(socket, secret='', flags=0, alg='sha256'):
     """receive pandas dataframe***"""
+
     z = socket.recv(flags)
     s = zlib.decompress(z)
     h = s[:s.find(b'\n')]
     p = s[(s.find(b'\n')+1):]
-    a = hashlib.new(alg)
-    a.update(secret.encode() + p)
 
-    if a.hexdigest().upper() != h.decode():
+    if h4mac(secret.encode(), p, alg).hexdigest().upper() != h.decode():
         raise Exception("Signature mismatch")
 
     return pickle.loads(p)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pandas-zmq",
-    version="0.0.2",
+    version="0.0.3",
     author="Ralph Ritoch",
     author_email="rritoch@gmail.com",
     description="Communicate Pandas DataFrame over ZeroMQ connection",
